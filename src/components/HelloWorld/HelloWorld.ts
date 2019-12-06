cb9d5ec438848d0d4ec64102f0421cdfc5c4bbd2
Steve Mbiele <mbielesteve@hotmail.com>
2019-12-06 16:51:29 -0500

    Fixed some css, and upgraded to Typescript ~3.7.2

--- src/components/HelloWorld/HelloWorld.ts
+++ src/components/HelloWorld/HelloWorld.ts
@@ -6,53 +6,52 @@ export default class HelloWorld extends Vue {
   private lstAdded: Array<string> = [];
 
   onDragStart = (event: any) => {
-    console.log("is is the on drag start");
     event.dataTransfer.setData("text/plain", event.target.id);
   };
 
   onDragOver = (event: any) => {
-    console.log("is int the drag over");
     event.preventDefault();
   };
 
   onDropInDropZone = (event: any) => {
     event.preventDefault();
-    console.log("is int the onDrop");
 
     const id = event.dataTransfer.getData("text");
     const draggableElement = document.getElementById(id);
     const dropzone = event.target;
 
-    var sourceClass = draggableElement!.className;
+    var sourceClass = draggableElement?.className;
     var targetId = dropzone.id;
-    var newNode = draggableElement!.cloneNode(true);
+    var newNode = draggableElement?.cloneNode(true);
     var newEle = <HTMLElement>newNode;
-    var elementsOfSameType = document
-      .getElementById(targetId)!
-      .getElementsByClassName(sourceClass);
+    if (sourceClass) {
+      var elementsOfSameType = document
+        .getElementById(targetId)!
+        .getElementsByClassName(sourceClass);
 
-    newEle.id = `${newEle.id}_${elementsOfSameType.length + 1}`;
-    newEle.addEventListener("dragstart", this.onDragStart);
+      newEle.id = `${newEle.id}_${elementsOfSameType.length + 1}`;
+      newEle.addEventListener("dragstart", this.onDragStart);
 
-    if (
-      draggableElement!.parentElement!.id !== dropzone.parentElement.id &&
-      draggableElement!.parentElement!.id != dropzone.id
-    ) {
-      dropzone.appendChild(newEle);
-      this.lstAdded.push(newEle.id);
-      console.log(this.lstAdded);
+      if (
+        draggableElement?.parentElement?.id !== dropzone.parentElement.id &&
+        draggableElement?.parentElement?.id != dropzone.id
+      ) {
+        dropzone.appendChild(newEle);
+        this.lstAdded.push(newEle.id);
+      }
+
+      event.dataTransfer.clearData();
     }
-
-    event.dataTransfer.clearData();
   };
 
   onDropInItemZone = (event: DragEvent) => {
     event.preventDefault();
-    const id = event.dataTransfer!.getData("text");
-    const element = document.getElementById(id);
-    this.lstAdded = this.removeFromArray(this.lstAdded, id);
-    element!.parentNode!.removeChild(<Node>element);
-    console.log(this.lstAdded);
+    const id = event.dataTransfer?.getData("text");
+    if (id) {
+      const element = document.getElementById(id);
+      this.lstAdded = this.removeFromArray(this.lstAdded, id);
+      element?.parentNode?.removeChild(<Node>element);
+    }
   };
 
   private removeFromArray(array: Array<any>, element: any): Array<any> {
